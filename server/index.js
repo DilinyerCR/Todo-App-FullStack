f3e00284c582caf6bd3e6df815e426d11be7c30c
DilinyerCR <dilinyerecr@mail.com>
2024-06-04 00:21:01 -0400

    testing vercel database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const server = require("./src/server");  
 const { database } = require('./src/db');
-const PORT = 3001;
+const PORT = 5432;
 
 
 //Esta parte es responsable de sincronizar los modelos definidos en tu aplicación con la base de datos y luego iniciar el servidor Express.
 database.sync().then(() => { 
     console.log("Database Connected!")
     //Aqui iniciamos el server.
-    server.listen(3001, () => {
+    server.listen(5432, () => {
         console.log(`Server is running at PORT: ${PORT}`);
     });
 });
