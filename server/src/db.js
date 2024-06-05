f5bedda8696a042a32d7d07311c656ab703c0c0c
DilinyerCR <dilinyerecr@mail.com>
2024-06-04 20:04:40 -0400

    trying vercel

--- server/src/db.js
+++ server/src/db.js
@@ -1,18 +1,19 @@
 // El archivo db.js es esencial en una aplicación que utiliza Sequelize para interactuar con una base de datos, ya que se encarga de establecer la conexión con la base de datos y de configurar Sequelize. Este archivo es crucial para la inicialización de la base de datos y la definición de los modelos que representan las tablas en la base de datos. 
-const pg = require('pg');
-require("dotenv").config();
+// const pg = require('pg');
+// require("dotenv").config();
 const { Sequelize } = require('sequelize');
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST } = process.env; //Llamo a mis variables de entorno.
+// const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST } = process.env; //Llamo a mis variables de entorno.
 const taskModel = require('./models/Task'); //Importo el modelo para las tareas.
 const userModel = require('./models/User'); //Importo el modelo para los usuarios.
 
 // Creamos una nueva instancia de Sequelize, que se utilizará para interactuar con la base de datos.
-const URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/verceldb?sslmode=require`;
+// const URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/verceldb?sslmode=require`;
+const URL = "postgres://default:cCHUQd8Kq3mw@ep-dry-bonus-a4vvclsb.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
 
 const database = new Sequelize(URL, {
     logging: false,
     native: false,
-    dialectModule: pg,
+    // dialectModule: pg,
 });
 
 
