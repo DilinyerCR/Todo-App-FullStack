a74ca829a2a93ba4069a1c9f462d97565bdf00ad
DilinyerCR <dilinyerecr@mail.com>
2024-06-07 16:36:14 -0400

    nothing

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ const router = require('./routes/routes');
 
 //Esto se le conoce como CORS, sin esto no se comunican el front y el back
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://todo-app-full-stack-mauve.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://todo-app-full-stack-mauve.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
        'Access-Control-Allow-Headers',
