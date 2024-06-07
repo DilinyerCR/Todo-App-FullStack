9de09bf5cc800c8056d6c97293719a2fda282892
DilinyerCR <dilinyerecr@mail.com>
2024-06-07 18:11:01 -0400

    deploying to RW

--- server/src/db.js
+++ server/src/db.js
@@ -1,13 +1,13 @@
 // El archivo db.js es esencial en una aplicación que utiliza Sequelize para interactuar con una base de datos, ya que se encarga de establecer la conexión con la base de datos y de configurar Sequelize. Este archivo es crucial para la inicialización de la base de datos y la definición de los modelos que representan las tablas en la base de datos.
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env; //Llamo a mis variables de entorno.
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env; //Llamo a mis variables de entorno.
 const taskModel = require('./models/Task'); //Importo el modelo para las tareas.
 const userModel = require('./models/User'); //Importo el modelo para los usuarios.
 
 // Creamos una nueva instancia de Sequelize, que se utilizará para interactuar con la base de datos.
 // const URL = `postgresql://${POSTGRES_USER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`; //Railwail
-const URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/todoapp`;  //LOCAL
+const URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;  //LOCAL
 
 const database = new Sequelize(URL, {
     logging: false,
