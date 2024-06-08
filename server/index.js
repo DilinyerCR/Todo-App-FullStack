6d8cf9a2ca8547d5bd9ef9c98d073935569aade0
DilinyerCR <dilinyerecr@mail.com>
2024-06-07 20:34:08 -0400

    deploying to RW

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const server = require("./src/server");  
 const { database } = require('./src/db');
-const PORT = 3001;
+require("dotenv").config();
+const { PORT } = process.env;
 
 
 //Esta parte es responsable de sincronizar los modelos definidos en tu aplicación con la base de datos y luego iniciar el servidor Express.
 database.sync().then(() => { 
     console.log("Database Connected!")
     //Aqui iniciamos el server.
-    server.listen(3001, () => {
+    server.listen(PORT, () => {
         console.log(`Server is running at PORT: ${PORT}`);
     });
 });
